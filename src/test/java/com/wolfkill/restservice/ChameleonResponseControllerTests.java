9367684dc2305a9f01ac08b90e40820aa1e74930
vadimm <vmelnikov@baltinfocom.ru>
2021-05-23 21:59:11 +0300

    Delete test

--- src/test/java/com/wolfkill/restservice/ChameleonResponseControllerTests.java
+++ src/test/java/com/wolfkill/restservice/ChameleonResponseControllerTests.java
@@ -31,22 +31,8 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class ChameleonResponseControllerTests {
 
-	// @Autowired
-	// private MockMvc mockMvc;
-	//
-	// @Test
-	// public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
-	//
-	// 	this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-	// 			.andExpect(jsonPath("$.content").value("Hello, World!"));
-	// }
-	//
-	// @Test
-	// public void paramGreetingShouldReturnTailoredMessage() throws Exception {
-	//
-	// 	this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-	// 			.andDo(print()).andExpect(status().isOk())
-	// 			.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
-	// }
+	@Test
+	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+	}
 
 }
