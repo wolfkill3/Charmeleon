b9d6c6e6d0054943b4f1e1787f638fe8936f8374
vadimm <vmelnikov@baltinfocom.ru>
2021-05-28 22:51:05 +0300

    Add wrapper for user profile

--- src/main/java/com/wolfkill/charmeleon/application/CharmeleonController.java
+++ src/main/java/com/wolfkill/charmeleon/application/CharmeleonController.java
@@ -3,6 +3,7 @@ package com.wolfkill.charmeleon.application;
 import java.util.logging.Logger;
 
 import com.wolfkill.charmeleon.application.db.AuthChecker;
+import com.wolfkill.charmeleon.application.user.UserData;
 import com.wolfkill.charmeleon.application.user.UserProperties;
 import com.wolfkill.charmeleon.application.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,29 +30,20 @@ public class CharmeleonController {
     public UserProperties authResponse(
         @RequestParam(value = "login") String login,
         @RequestParam(value = "password") String password) {
-        logger.info("login = " + login);
-        logger.info("password = " + password);
         CharmeleonAuthResponse authResponse = new CharmeleonAuthResponse(login, password);
         return AuthChecker.checkAuthResponse(authResponse, userRepository);
     }
 
-    @PostMapping(path = "/add")
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
     public @ResponseBody
-    String addNewUser(@RequestParam String link
-        , @RequestParam String login
-        , @RequestParam String password) {
-
-        UserProperties userProperties = new UserProperties();
-        userProperties.setLink(link);
-        userProperties.setLogin(login);
-        userProperties.setPassword(password);
-        userRepository.save(userProperties);
+    String addNewUser(@RequestBody UserData userData) {
+        userRepository.save(userData);
         return "Saved";
     }
 
     @GetMapping(path = "/all")
     public @ResponseBody
-    Iterable<UserProperties> getAllUsers() {
+    Iterable<UserData> getAllUsers() {
         return userRepository.findAll();
     }
 }
--- src/main/java/com/wolfkill/charmeleon/application/db/AuthChecker.java
+++ src/main/java/com/wolfkill/charmeleon/application/db/AuthChecker.java
@@ -1,10 +1,11 @@
 package com.wolfkill.charmeleon.application.db;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.wolfkill.charmeleon.application.CharmeleonAuthResponse;
+import com.wolfkill.charmeleon.application.ResponseStatus;
+import com.wolfkill.charmeleon.application.user.UserData;
 import com.wolfkill.charmeleon.application.user.UserProperties;
 import com.wolfkill.charmeleon.application.user.UserRepository;
 
@@ -12,18 +13,20 @@ public class AuthChecker {
     private static Logger logger = Logger.getLogger(AuthChecker.class.getName());
 
     public static UserProperties checkAuthResponse(CharmeleonAuthResponse authResponse, UserRepository userRepository) {
-        UserProperties user = null;
-        for (UserProperties userProperties : userRepository.findAll()) {
-            if (authResponse.getLogin().equals(userProperties.getLogin())
-                && authResponse.getPassword().equals(userProperties.getPassword())) {
-                user = userProperties;
+        UserProperties userProperties = new UserProperties();
+        for (UserData userData : userRepository.findAll()) {
+            if (authResponse.getLogin().equals(userData.getLogin())
+                && authResponse.getPassword().equals(userData.getPassword())) {
+                userProperties.userData = userData;
             }
         }
-        if (user != null) {
+        if (userProperties.userData != null) {
             logger.info("Пользователь найден");
+            userProperties.responseStatus = ResponseStatus.CONFIRMED;
         } else {
             logger.log(Level.WARNING, "Пользователь не найден");
+            userProperties.responseStatus = ResponseStatus.NOT_CONFIRMED;
         }
-        return user;
+        return userProperties;
     }
 }
